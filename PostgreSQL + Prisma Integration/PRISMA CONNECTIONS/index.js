2d6fe2284975837e4f6e65e3e258538005d82d9d
khadija-099 <khadijabatool780@gmail.com>
2025-07-11 14:40:44 -0700

    push

--- PostgreSQL + Prisma Integration/PRISMA CONNECTIONS/index.js
+++ PostgreSQL + Prisma Integration/PRISMA CONNECTIONS/index.js
@@ -1,36 +1,18 @@
+
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-    // #create
     const newUser = await prisma.user.create({
-        data: { name: "Khadija", email: "khadija@example.com"}
+        data: {
+            name: "Sadia",
+            email: "Sadia@yahoo.com",
+            password: "Sadia122"
+        }
     });
-    console.log("Created:", newUser)
-
-    // READ
-    const users = await prisma.user.findMany();
-    console.log("All Users:", users)
-
-    // UPDATE
-    const updated = await prisma.user.update({
-        where: {id: newUser.id},
-        data: {name: "Khadija Updated"}
-    });
-    console.log("Updated:", updated)
-
-    // DELETE
-    const deleted = await prisma.user.delete({
-        where: {id: newUser.id}
-    });
-    console.log("Deleted:", deleted)
-
+    console.log("Inserted User:", newUser)
 }
 
 main()
-    cath(console.error)
-    .finally(() => prisma.$disconnect());
-
-
-// npm install @prisma/client    
-// node index.js
+    .catch(console.error)
+    .finally(()=> prisma.$disconnect());
